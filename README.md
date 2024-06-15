61a15a01c8f71047caa728c33a366ae46262326e
Davi Johnson <hello@devdavi.com>
2024-06-15 10:25:17 -0500

    Added LSP support

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Davi's basic Neo-vim configuration
 
 ## Overview
----
 You probably don't want to use this, but you can take a look and be a creeper
---
+++ lua/plugins/lsp-plugins.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "somesass_ls", "html", "cssls", "css_variables", "tsserver"}
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.somesass_ls.setup({})
+      lspconfig.css_variables.setup({})
+      lspconfig.tsserver.setup({})
+
+
+      vim.keymap.set('n', 'K',vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd',vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+
+}
